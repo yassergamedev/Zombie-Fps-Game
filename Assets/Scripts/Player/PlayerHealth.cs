0b4d1fe3e77eb248a87da1a089b5a4e4a7e332c4
Ysser Mez <114354646+yassergamedev@users.noreply.github.com>
2024-08-16 17:36:13 +0100

    Damage Overlay + Heal

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Header("HealthBar")]
     public Slider healthSlider;      // UI Slider for current health
     public Slider backHealthSlider;  // UI Slider for delayed health
 
@@ -13,10 +14,17 @@ public class PlayerHealth : MonoBehaviour
     public float maxHealth = 100f;
     public float chipSpeed = 2f;
 
+    [Header("Damage Overlay")]
+    public Image overlay;
+    public float duration; //how long the image stays fully opaque
+    public float fadeSpeed; //how quickly the image will fade
+
+    private float durationTimer; //Timer to check against the duration
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0);
     }
 
     // Update is called once per frame
@@ -24,16 +32,25 @@ public class PlayerHealth : MonoBehaviour
     {
         health = Mathf.Clamp(health, 0, maxHealth);
         UpdateHealthUI();
-        if (Input.GetKeyDown(KeyCode.Y))
+
+        if(overlay.color.a > 0)
         {
-            TakeDamage(Random.Range(0, maxHealth));
-            
+            if (health < 30)
+                return;
+            durationTimer += Time.deltaTime;
+            if(durationTimer > duration)
+            {
+                // fade the image
+                float tempAlpha = overlay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
+                overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, tempAlpha);
+
+            }
         }
     }
 
     public void UpdateHealthUI()
     {
-        
         float fillA = healthSlider.value;
         float fillB = backHealthSlider.value;
         float hFraction = health / maxHealth;
@@ -45,11 +62,26 @@ public class PlayerHealth : MonoBehaviour
             float percentComplete = lerpTimer / chipSpeed;
             backHealthSlider.value = Mathf.Lerp(fillB, hFraction, percentComplete);
         }
+        else if (fillB < hFraction)
+        {
+            healthSlider.value = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            backHealthSlider.value = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
     }
-
     public void TakeDamage(float damage)
     {
         health -= damage;
         lerpTimer = 0f;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 1);
+        durationTimer = 0f;
+    }
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        lerpTimer = 0f;
+        
     }
 }
